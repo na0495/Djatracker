5988b451908bbf18284081e47114233f9e60c654
na4095 <saadmrabet008@gmail.com>
2021-05-08 07:25:12 +0000

    adding view & form

---
+++ djatracker/data/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import store
+
+
+class StoreModelForm(forms.ModelForm):
+    class Meta:
+        model = store
+        fields = ('destination_point', 'travel_mode',)
--- djatracker/data/views.py
+++ djatracker/data/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import store
 
 # Create your views here.
+
+
+def calculate_distance_view(request):
+    object = get_object_or_404(store, id=1)
+    context = {
+        'distance': object
+    }
+    return render(request, 'store/main.html', context)
